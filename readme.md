280879692d78272bd3f5d3671a796ab3713afa68
Chris Duell <me@chrisduell.com>
2013-03-28 22:14:44 +1100

    More insightful output for field and values in the case where the field was a foreign key

--- readme.md
+++ readme.md
@@ -20,6 +20,8 @@ use Venturecraft\Revisionable\Revisionable;
 class Article extends Revisionable { }
 ````
 
+If needed, you can disable the revisioning by setting `$revisionEnabled` to false in your model. This can be handy if you want to temporarily disable revisioning, or if you want to create your own base model that extends revisionable, which all of your models extend, but you want to turn revisionable off for certain models.
+
 For any models you don't wish to keep a revision history for, we recommend extending Ardent (which is also installed when revisionable is installed via composer), e.g.,
 
 ````
@@ -40,6 +42,42 @@ $article = Article::find($id);
 $history = $article->revisionHistory;
 ````
 
+### Displaying history
+
+For the most part, the revision history will hold enough information to directly output a change history, however in the cases where a foreign key is updated we need to be able to do some mapping and display something nicer than `plan_id changed from 3 to 1`.
+
+To help with this, there's a few helper methods to display more insightful information, so you can display something like `James changed plan from bronze to gold'.
+
+The above would be the result from this:
+````
+@foreach($account->revisionHistory as $history )
+    <li>{{ $history->userResponsible()->first_name }} changed {{ $history->itemField() }} from {{ $history->oldValue() }} to {{ $history->newValue() }}</li>
+@endforeach
+````
+
+#### userResponsible()
+
+Returns the User that was responsible for making the revision. A user model is returned, or null if there was no user recorded.
+
+#### itemField()
+
+Returns the name of the field that was updated, if the field that was updated was a foreign key (at this stage, it simply looks to see if the field has the suffix of `_id`) then the text before `_id` is returned. e.g., if the field was `plan_id`, then `plan` would be returned.
+
+#### identifiableName()
+
+This is used when the value (old or new) is the id of a foreign key relationship.
+
+By default, it simply returns the ID of the model that was updated. It is up to you to override this method in your own models to return something meaningful. e.g.,
+````
+public function identifiableName()
+{
+    return $this->title;
+}
+````
+
+#### oldValue() and newValue()
+
+Get the value of the model before or after the update. If it was a foreign key, identifiableName() is called.
 
 ## Contributing
 
--- src/Venturecraft/Revisionable/Revision.php
+++ src/Venturecraft/Revisionable/Revision.php
@@ -1,14 +1,125 @@
 <?php namespace Venturecraft\Revisionable;
 
+/**
+ * Revision
+ *
+ * Base model to allow for revision history on
+ * any model that extends this model
+ *
+ * (c) Venture Craft <http://www.venturecraft.com.au>
+ */
+
 class Revision extends \Eloquent {
 
 
 	public $table = 'revisions';
 
 
+    /**
+     * Revisionable
+     * Grab the revision history for the model that is calling
+     * @return array revision history
+     */
     public function revisionable()
     {
         return $this->morphTo();
     }
+
+
+
+    /**
+     * Identifiable Name
+     * When displaying revision history, when a foreigh key is updated
+     * instead of displaying the ID, you can choose to display a string
+     * of your choice, just override this method in your model
+     * By default, it will fall back to the models ID.
+     * @return string an identifying name for the model
+     */
+    public function identifiableName()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Item Field
+     * Returns the field that was updated, in the case that it's a foreighn key
+     * denoted by a suffic of "_id", then "_id" is simply stripped
+     * @return string field
+     */
+    public function itemField()
+    {
+        if (strpos($this->key, '_id')) {
+            return str_replace('_id', '', $this->key);
+        } else {
+            return $this->key;
+        }
+    }
+
+
+
+    /**
+     * Old Value
+     * Grab the old value of the field, if it was a foreign key
+     * attempt to get an identifying name for the model
+     * @return string old value
+     */
+    public function oldValue()
+    {
+        try {
+            if (strpos($this->key, '_id')) {
+                $model = str_replace('_id', '', $this->key);
+                $item = $model::find($this->old_value);
+                return $item->identifiableName();
+            }
+        } catch (Exception $e) {
+            // Just a failsafe, in the case the data setup isn't as expected
+            // Nothing to do here.
+        }
+
+        // if there was an issue
+        // or, if it's a normal value
+        return $this->old_value;
+
+    }
+
+
+
+    /**
+     * New Value
+     * Grab the new value of the field, if it was a foreign key
+     * attempt to get an identifying name for the model
+     * @return string old value
+     */
+    public function newValue()
+    {
+        try {
+            if (strpos($this->key, '_id')) {
+                $model = str_replace('_id', '', $this->key);
+                $item = $model::find($this->new_value);
+                return $item->identifiableName();
+            }
+        } catch (Exception $e) {
+            // Just a failsafe, in the case the data setup isn't as expected
+            // Nothing to do here.
+        }
+
+        // if there was an issue
+        // or, if it's a normal value
+        return $this->new_value;
+
+    }
+
+
+    /**
+     * User Responsible
+     * @return User user responsible for the change
+     */
+    public function userResponsible()
+    {
+        return \User::find($this->user_id);
+    }
+
+
 
 }
