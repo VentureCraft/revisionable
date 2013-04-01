aa6a266c0c3ac9d54c6951d0e859573d8a6bc352
Chris Duell <me@chrisduell.com>
2013-04-01 16:55:27 +1100

    Support for specifying which fields to keep a revision of for the model
    Signed-off-by: Chris Duell <me@chrisduell.com>

--- src/Venturecraft/Revisionable/Revisionable.php
+++ src/Venturecraft/Revisionable/Revisionable.php
@@ -11,11 +11,24 @@ use Illuminate\Support\ServiceProvider;
 
 class Revisionable extends \Eloquent {
 
-	private $original_data;
-    private $updated_data;
+	private $originalData;
+    private $updatedData;
     private $updating;
 
     protected $revisionEnabled = true;
+
+    /**
+     * A list of fields that should have
+     * revisions kept for the model.
+     */
+    protected $keepRevisionOf = array();
+
+    /**
+     * A list of fields that should be ignored when keeping
+     * revisions of the model.
+     */
+    protected $dontKeepRevisionOf = array();
+
 
     public function __construct(array $attributes = array())
     {
@@ -59,15 +72,15 @@ class Revisionable extends \Eloquent {
     {
 
         if ($this->revisionEnabled) {
-        	$this->original_data 	= $this->original;
-        	$this->updated_data 	= $this->attributes;
+        	$this->originalData 	= $this->original;
+        	$this->updatedData 	= $this->attributes;
 
             // we can only safely compare basic items,
             // so for now we drop any object based items, like DateTime
-            foreach ($this->updated_data as $key => $val) {
+            foreach ($this->updatedData as $key => $val) {
                 if (gettype($val) == 'object') {
-                    unset($this->original_data[$key]);
-                    unset($this->updated_data[$key]);
+                    unset($this->originalData[$key]);
+                    unset($this->updatedData[$key]);
                 }
             }
 
@@ -92,16 +105,16 @@ class Revisionable extends \Eloquent {
 		if($this->revisionEnabled AND $this->updating) {
 			// if it does, it means we're updating
 
-			$changes = array_diff($this->updated_data, $this->original_data);
+            $changes_to_record = $this->changedRevisionableFields();
 
-			foreach( $changes as $key => $change ) {
+			foreach( $changes_to_record as $key => $change ) {
 
 				$revision = new Revision();
 				$revision->revisionable_type 	= get_class($this);
 				$revision->revisionable_id 		= $this->id;
 				$revision->key 					= $key;
-				$revision->old_value			= $this->original_data[$key];
-				$revision->new_value 			= $this->updated_data[$key];
+				$revision->old_value			= $this->originalData[$key];
+				$revision->new_value 			= $this->updatedData[$key];
 				$revision->user_id 				= \Auth::user()->id;
 				$revision->save();
 
@@ -111,5 +124,50 @@ class Revisionable extends \Eloquent {
 
     }
 
+
+    /**
+     * Get all of the changes that have been made, that are also supposed
+     * to have their changes recorded
+     * @return array fields with new data, that should be recorded
+     */
+    private function changedRevisionableFields()
+    {
+
+        $changes = array_diff($this->updatedData, $this->originalData);
+
+        $changes_to_record = array();
+        foreach ($changes as $key => $value) {
+            if ($this->isRevisionable($key))  {
+                $changes_to_record[$key] = $value;
+            } else {
+                // we don't need these any more, and they could
+                // contain a lot of data, so lets trash them.
+                unset($this->updatedData[$key]);
+                unset($this->originalData[$key]);
+            }
+        }
+
+
+        return $changes_to_record;
+
+    }
+
+    /**
+     * Check if this field should have a revision kept
+     * @param  string  $key
+     * @return boolean
+     */
+    private function isRevisionable($key)
+    {
+
+        // If the field is explicitly revisionable, then return true.
+        // If it's explicitly not revisionable, return false.
+        // Otherwise, if neither condition is met, only return true if
+        // we aren't specifying revisionable fields.
+        if (in_array($key, $this->keepRevisionOf)) return true;
+        if (in_array($key, $this->dontKeepRevisionOf)) return false;
+
+        return empty($this->keepRevisionOf);
+    }
 
 }
