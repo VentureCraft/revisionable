ba97158fc895736fc280eff7dc276d4b26d5c2b2
Paulo Freitas <me@paulofreitas.me>
2014-08-22 09:51:47 -0300

    Fixed wrong Sentinel FQCN.

--- src/Venturecraft/Revisionable/RevisionableTrait.php
+++ src/Venturecraft/Revisionable/RevisionableTrait.php
@@ -168,7 +168,7 @@ trait RevisionableTrait
     {
         try {
             if (class_exists($class = '\Cartalyst\Sentry\Facades\Laravel\Sentry')
-                    || class_exists($class = '\Cartalyst\Sentinel\Facades\Laravel\Sentinel')) {
+                    || class_exists($class = '\Cartalyst\Sentinel\Laravel\Facades\Sentinel')) {
                 return ($class::check()) ? $class::getUser()->id : null;
             } elseif (\Auth::check()) {
                 return \Auth::user()->getAuthIdentifier();
