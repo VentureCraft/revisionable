ddb8638fe4c33efcdcb5468182d027ef2dc6adef
Johan Steen <artstorm@gmail.com>
2015-09-07 02:52:21 +0700

    Implements belongsToManyRevisionable to replace sync event

--- src/Venturecraft/Revisionable/RevisionableTrait.php
+++ src/Venturecraft/Revisionable/RevisionableTrait.php
@@ -44,32 +44,6 @@ trait RevisionableTrait
     protected $dirtyData = array();
 
     /**
-     * Register a syncing model event with the dispatcher.
-     *
-     * @param  \Closure|string  $callback
-     * @param  int  $priority
-     *
-     * @return void
-     */
-    public static function syncing($callback, $priority = 0)
-    {
-        static::registerModelEvent('syncing', $callback, $priority);
-    }
-
-    /**
-     * Register a synced model event with the dispatcher.
-     *
-     * @param  \Closure|string  $callback
-     * @param  int  $priority
-     *
-     * @return void
-     */
-    public static function synced($callback, $priority = 0)
-    {
-        static::registerModelEvent('synced', $callback, $priority);
-    }
-
-    /**
      * Ensure that the bootRevisionableTrait is called only
      * if the current installation is a laravel 4 installation
      * Laravel 5 will call bootRevisionableTrait() automatically
@@ -90,16 +64,6 @@ trait RevisionableTrait
      */
     public static function bootRevisionableTrait()
     {
-        static::syncing(function ($model, $relation) {
-            $model->preSync($relation);
-            return true;
-        });
-
-        static::synced(function ($model, $relation, $ids) {
-            $model->postSync($relation, $ids);
-            return true;
-        });
-
         static::saving(function ($model) {
             $model->preSave();
         });
@@ -157,6 +121,50 @@ trait RevisionableTrait
         $localKey = $localKey ?: $this->getKeyName();
 
         return new HasManyRevisionable($instance->newQuery(), $this, $instance->getTable().'.'.$foreignKey, $localKey, $relation);
+    }
+
+    /**
+     * Define a many-to-many relationship that can track revisions.
+     *
+     * @param  string  $related
+     * @param  string  $table
+     * @param  string  $foreignKey
+     * @param  string  $otherKey
+     * @param  string  $relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function belongsToManyRevisionable($related, $table = null, $foreignKey = null, $otherKey = null, $relation = null)
+    {
+        // If no relationship name was passed, we will pull backtraces to get the
+        // name of the calling function. We will use that function name as the
+        // title of this relation since that is a great convention to apply.
+        if (is_null($relation)) {
+            $relation = $this->getBelongsToManyCaller();
+        }
+
+        // First, we'll need to determine the foreign key and "other key" for the
+        // relationship. Once we have determined the keys we'll make the query
+        // instances as well as the relationship instances we need for this.
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $instance = new $related;
+
+        $otherKey = $otherKey ?: $instance->getForeignKey();
+
+        // If no table name was provided, we can guess it by concatenating the two
+        // models using underscores in alphabetical order. The two model names
+        // are transformed to snake case from their default CamelCase also.
+        if (is_null($table)) {
+            $table = $this->joiningTable($related);
+        }
+
+        // Now we're ready to create a new query builder for the related model and
+        // the relationship instances for the relation. The relations will set
+        // appropriate query constraint and entirely manages the hydrations.
+        $query = $instance->newQuery();
+
+        return new BelongsToManyRevisionable($query, $this, $table, $foreignKey, $otherKey, $relation);
     }
 
     /**
