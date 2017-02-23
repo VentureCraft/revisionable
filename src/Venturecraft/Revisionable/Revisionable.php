ab5b4af215bd47059abf737be0fcddab8ed87caa
Marek Toth <scorpion909@gmail.com>
2017-02-23 13:40:15 +0100

    adding unique revision identifier

--- src/Venturecraft/Revisionable/Revisionable.php
+++ src/Venturecraft/Revisionable/Revisionable.php
@@ -138,11 +138,13 @@ class Revisionable extends Eloquent
             $changes_to_record = $this->changedRevisionableFields();
 
             $revisions = array();
-
+            $uniqid = uniqid();
+            
             foreach ($changes_to_record as $key => $change) {
                 $revisions[] = array(
                     'revisionable_type'     => $this->getMorphClass(),
                     'revisionable_id'       => $this->getKey(),
+                    'revision_id'           => $uniqid,
                     'key'                   => $key,
                     'old_value'             => array_get($this->originalData, $key),
                     'new_value'             => $this->updatedData[$key],
---
+++ src/migrations/2017_02_23_062329_update_revisions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRevisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('revisions', function (Blueprint $table) {
+            $table->string('revision_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('revisions', function (Blueprint $table) {
+            $table->dropColumn('revision_id');
+        });
+    }
+}
