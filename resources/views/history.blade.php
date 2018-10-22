89f0590676089dfa9e41103b50444fc2cedae9c1
Tarek Adam <tarek.adam@gmail.com>
2018-10-22 16:17:01 -0600

    tweaks

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,6 +1,6 @@
 @foreach($model->revisionHistory as $history)
-    @if($history->key == 'created_at' && !$history->old_value)
-        <div>{{ $history->userResponsible()->first_name }} created this resource at {{ $history->newValue() }}</div>
+    @if($history->key == 'created_at' and !$history->old_value)
+        <div>{{ (!empty($history->userResponsible()))? $history->userResponsible()->first_name . ' created' : 'system created' }} this resource at {{ $history->newValue() }}</div>
     @else
         <div>{{ $history->userResponsible()->first_name }} changed {{ $history->fieldName() }} from {{ $history->oldValue() }} to {{ $history->newValue() }}</div>
     @endif
