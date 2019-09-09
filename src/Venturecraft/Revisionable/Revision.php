a9f592bdf0e09f932d45561f39047a629150e07e
Andrew Judd <awjudd@gmail.com>
2019-09-09 17:31:41 -0400

    Removing the dependency on array_get
    Resolving issue with deprecated string function

--- src/Venturecraft/Revisionable/Revision.php
+++ src/Venturecraft/Revisionable/Revision.php
@@ -2,6 +2,7 @@
 
 namespace Venturecraft\Revisionable;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Support\Facades\Log;
 
@@ -163,7 +164,7 @@ class Revision extends Eloquent
                     // Check if model use RevisionableTrait
                     if(method_exists($item, 'identifiableName')) {
                         // see if there's an available mutator
-                        $mutator = 'get' . studly_case($this->key) . 'Attribute';
+                        $mutator = 'get' . Str::studly($this->key) . 'Attribute';
                         if (method_exists($item, $mutator)) {
                             return $this->format($item->$mutator($this->key), $item->identifiableName());
                         }
@@ -179,7 +180,7 @@ class Revision extends Eloquent
             // if there was an issue
             // or, if it's a normal value
 
-            $mutator = 'get' . studly_case($this->key) . 'Attribute';
+            $mutator = 'get' . Str::studly($this->key) . 'Attribute';
             if (method_exists($main_model, $mutator)) {
                 return $this->format($this->key, $main_model->$mutator($this->$which_value));
             }
