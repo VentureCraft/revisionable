0b80c8e1a88d7791ed0ecdae2eb2fadc4c5952bc
Robbie Love <Robbie@ubiquilife.com>
2019-10-17 19:09:50 +1000

    update uuid migration to seed uuid and work correctly to remove id column and add uid column

--- src/migrations/2019_10_16_222329_change_id_to_uid_revisions_table.php
+++ src/migrations/2019_10_16_222329_change_id_to_uid_revisions_table.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
-class CreateRevisionsTable extends Migration
+class ChangeIdToUidRevisionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,24 +13,45 @@ class CreateRevisionsTable extends Migration
     public function up()
     {
         Schema::table('revisions', function ($table) {
-            $column = 'id';
-            if(Schema::hasColumn($table->getTable(), $column))
-            {
-                $table->dropPrimary([$column]); //remove the primary key
-                $table->dropColumn($column);
-                echo "  [Dropped column '".$column."' from ".$table->getTable().".\n";
-            }
             
             $column = 'uid';
             if(!Schema::hasColumn($table->getTable(), $column))
             {
                 $table->uuid($column)->first();
+                echo "  [Added column '".$column."' to ".$table->getTable().".]\n";
+            }
+        });
+        Schema::table('revisions', function ($table) {
+            $column = 'uid';
+            if(Schema::hasColumn($table->getTable(), $column))
+            {
+                echo "  [adding uuid's]... (might take a while)\n";
+                foreach (DB::table($table->getTable())->get('id') as $k => $v) {
+                    DB::table($table->getTable())->where('id', '=', $v->id)->update(['uid' => Str::uuid()]);
+                }
+            }
+            
+            
+            $column = 'id';
+            if(Schema::hasColumn($table->getTable(), $column))
+            {
+                $table->integer($column)->unsigned()->change();
+                $table->dropPrimary([$column]); //remove the primary key
+                $table->dropColumn($column);
+                echo "  [Dropped column '".$column."' from ".$table->getTable().".]\n";
+            }
+        });
+        Schema::table('revisions', function ($table) {
+            
+            $column = 'uid';
+            if(Schema::hasColumn($table->getTable(), $column))
+            {
                 $table->primary($column);
-                echo "  [Added column '".$column."' to ".$table->getTable().".\n";
+                echo "  [primary column '".$column."' added to ".$table->getTable().".]\n";
             }
         });
     }
-
+    
     /**
      * Reverse the migrations.
      *
@@ -43,14 +65,16 @@ class CreateRevisionsTable extends Migration
             {
                 $table->dropPrimary([$column]); //remove the primary key
                 $table->dropColumn($column);
-                echo "  [Dropped column '".$column."' from ".$table->getTable().".\n";
+                echo "  [Dropped column '".$column."' from ".$table->getTable().".]\n";
             }
+        });
         
+        Schema::table('revisions', function ($table) {
             $column = 'id';
             if(!Schema::hasColumn($table->getTable(), $column))
             {
                 $table->increments($column)->first();
-                echo "  [Added column '".$column."' to ".$table->getTable().".\n";
+                echo "  [Added column '".$column."' to ".$table->getTable().".]\n";
             }
         });
     }
