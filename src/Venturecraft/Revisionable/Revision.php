f05fa11974f8fa7ccfd3246ebd178676ef399c19
Julian Tekook <julian@tekook.de>
2021-09-30 15:07:38 +0200

    add the old resolving back so this does not break old resolving

--- src/Venturecraft/Revisionable/Revision.php
+++ src/Venturecraft/Revisionable/Revision.php
@@ -238,7 +238,28 @@ class Revision extends Eloquent
      */
     public function userResponsible()
     {
-        return $this->user;
+        if($this->user_type != null) {
+            return $this->user;
+        }
+        if (empty($this->user_id)) { return false; }
+        if (class_exists($class = '\Cartalyst\Sentry\Facades\Laravel\Sentry')
+            || class_exists($class = '\Cartalyst\Sentinel\Laravel\Facades\Sentinel')
+        ) {
+            return $class::findUserById($this->user_id);
+        } else {
+            $user_model = app('config')->get('auth.model');
+
+            if (empty($user_model)) {
+                $user_model = app('config')->get('auth.providers.users.model');
+                if (empty($user_model)) {
+                    return false;
+                }
+            }
+            if (!class_exists($user_model)) {
+                return false;
+            }
+            return $user_model::find($this->user_id);
+        }
     }
 
     /**
